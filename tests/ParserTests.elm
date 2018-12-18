976817b54898c2207bca3e6a6bc35a904847de7f
Dave Keen <dev@ruffness.com>
2018-12-18 12:58:28 +0100

    Initial commit

---
+++ tests/ParserTests.elm
@@ -0,0 +1,59 @@
+module ParserTests exposing (suite)
+
+import Eexl.Ast exposing (..)
+import Eexl.Parser exposing (parse)
+import Expect exposing (Expectation)
+import Fuzz exposing (Fuzzer, int, list, string)
+import Test exposing (..)
+
+
+suite : Test
+suite =
+    describe "Eexl"
+        [ literalTests
+        , unaryOpTests
+        , binaryOpTests
+        , complexTests
+        ]
+
+
+literalTests : Test
+literalTests =
+    describe "literals"
+        [ test "true" <|
+            \_ -> Expect.equal (Ok <| Literal <| Bool True) (parse "true")
+        , test "false" <|
+            \_ -> Expect.equal (Ok <| Literal <| Bool False) (parse "false")
+        , fuzz int "int" <|
+            \n -> Expect.equal (Ok <| Literal <| Int n) (parse (String.fromInt n))
+        , test "var" <|
+            \_ -> Expect.equal (Ok <| Literal <| Var "someVar") (parse "someVar")
+        ]
+
+
+unaryOpTests : Test
+unaryOpTests =
+    describe "unaryOp"
+        [ test "not bool" <|
+            \_ -> Expect.equal (Ok <| UnaryOp <| Not <| Literal <| Bool True) (parse "!true")
+        , test "false" <|
+            \_ -> Expect.equal (Ok <| UnaryOp <| Not <| Literal <| Var "x") (parse "!x")
+        , test "multiple" <|
+            \_ -> Expect.equal (Ok <| UnaryOp <| Not <| UnaryOp <| Not <| Literal <| Var "x") (parse "!!x")
+        ]
+
+
+binaryOpTests : Test
+binaryOpTests =
+    only <|
+        describe "binaryOp"
+            [ test "eq" <|
+                \_ -> Expect.equal (Ok <| BinaryOp <| Eq (Literal <| Int 1) (Literal <| Int 2)) (parse "1 == 2")
+            ]
+
+
+complexTests : Test
+complexTests =
+    skip <|
+        describe "complex"
+            []
